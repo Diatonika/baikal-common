26be5aa984a5bba41e2695d8239d85085c54ec49
Valery Ratsko <zitrasa@yandex.ru>
2025-06-27 02:13:26 +0300

    feat(trade)!: rename OHLC(V) fields and allow nullables (#4)

--- src/baikal/common/trade/models/ohlc.py
+++ src/baikal/common/trade/models/ohlc.py
@@ -1,10 +1,11 @@
+from pandera import Field
 from polars import Float64
 
 from baikal.common.trade.models.time_series import TimeSeries
 
 
 class OHLC(TimeSeries):
-    open_price: Float64
-    high_price: Float64
-    low_price: Float64
-    close_price: Float64
+    open: Float64 = Field(nullable=True)
+    high: Float64 = Field(nullable=True)
+    low: Float64 = Field(nullable=True)
+    close: Float64 = Field(nullable=True)
