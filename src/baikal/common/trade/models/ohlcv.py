f8c4a19322fafb297239660a0d82a108bdb0ccc0
Valery Ratsko <zitrasa@yandex.ru>
2025-06-27 02:11:52 +0300

    feat(trade)!: rename OHLC(V) fields and allow nullables

--- src/baikal/common/trade/models/ohlcv.py
+++ src/baikal/common/trade/models/ohlcv.py
@@ -1,7 +1,8 @@
+from pandera import Field
 from polars import Float64
 
 from baikal.common.trade.models.ohlc import OHLC
 
 
 class OHLCV(OHLC):
-    volume: Float64
+    volume: Float64 = Field(nullable=True)
