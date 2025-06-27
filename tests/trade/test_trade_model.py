8476f1a6a1ef248ba578df637342903d0637cdf5
Valery Ratsko <zitrasa@yandex.ru>
2025-06-27 19:17:09 +0300

    feat(trade): support TradeModel to Polars Schema conversion (#5)

--- tests/trade/test_trade_model.py
+++ tests/trade/test_trade_model.py
@@ -1,3 +1,5 @@
+from polars import Datetime, Float64, Int64
+
 from baikal.common.trade.models import TradeModel
 
 
@@ -7,3 +9,16 @@ def test_column_names() -> None:
         second_column: int
 
     assert TestTradeModel.column_names() == ("first_column", "second_column")
+
+
+def test_polar_schema() -> None:
+    class TestTradeModel(TradeModel):
+        first_column: int
+        second_column: Float64
+        third_column: Datetime
+
+    assert TestTradeModel.polar_schema() == {
+        "first_column": Int64,
+        "second_column": Float64,
+        "third_column": Datetime(),
+    }
