0b975d07693c7064d10ca22b4b8650af7765930f
Valery Ratsko <zitrasa@yandex.ru>
2025-07-09 20:14:50 +0300

    feat(trade): make OHLCV non-nullable and present basic sequential parquet dataset TimeSeries writer (#7)
    * feat(trade): make OHLCV non-nullable and present basic sequential parquet dataset TimeSeries writer
    
    * return explicit polars dependency

--- src/baikal/common/trade/models/ohlc.py
+++ src/baikal/common/trade/models/ohlc.py
@@ -1,11 +1,10 @@
-from pandera import Field
 from polars import Float64
 
 from baikal.common.trade.models.time_series import TimeSeries
 
 
 class OHLC(TimeSeries):
-    open: Float64 = Field(nullable=True)
-    high: Float64 = Field(nullable=True)
-    low: Float64 = Field(nullable=True)
-    close: Float64 = Field(nullable=True)
+    open: Float64
+    high: Float64
+    low: Float64
+    close: Float64
